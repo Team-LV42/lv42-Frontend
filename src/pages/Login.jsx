3b24e097e09e6294056393e458a6b0daec61c267
lm9204 <yeondeok99@naver.com>
2024-02-28 20:12:17 +0000

    fix: change setter to selectorFamily #5

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -8,18 +8,17 @@ import { Link } from 'react-router-dom';
 
 const loginUrl="https://api.intra.42.fr/oauth/authorize?client_id=u-s4t2ud-a74331b456cc2db0bfb71c584a1d8b8cc6369d5c8b8f775d59a19e6483b9ddbd&redirect_uri=http%3A%2F%2F13.124.198.32%3A4242%2Fcallback&response_type=code";
 
-export const UserLoginState = atom({
-	key: 'UserLoginState',
-	default: '',
-});
+// export const UserLoginState = atom({
+// 	key: 'UserLoginState',
+// 	default: '',
+// });
 
 function Login() {
-	const [userState, setUserState] = useRecoilState(UserLoginState);
+	// const userState = useRecoilValue(UserLoginState);
 
 	return (
 		<div>
-			{/* <p>{userTestBlock}</p> */}
-			<button value={userState} class="login-button">
+			<button className="login-button">
 				<Link to={loginUrl}>Login</Link>
 			</button>
 		</div>
