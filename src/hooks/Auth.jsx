bbcdea69efeda4f0f7fc36718b5bca26b3814fb0
sehwanii <sehwan1023@gmail.com>
2024-03-06 12:54:30 +0000

    feat: add refreshToken api

--- src/hooks/Auth.jsx
+++ src/hooks/Auth.jsx
@@ -7,9 +7,10 @@ import {
 	useRecoilState,
 } from 'recoil';
 import { Link, useSearchParams } from 'react-router-dom';
-
+import { moment } from 'moment';
+import { CookiesProvieder } from 'react-cookie';
 const url = "http://54.180.96.16:3000/login?code=";
-const testUrl = "http://13.124.198.32:3000/login?code=";
+const testUrl = "http://54.180.96.16:4242/auth/login?code=";
 
 const codeState = atom({
 	key: 'codeState',
@@ -18,7 +19,7 @@ const codeState = atom({
 
 const UserLoginQuery = selectorFamily({
 	key: 'UserLoginQuery',
-	get: ( code ) => async () => {
+	get: (code) => async () => {
 		try {
 			const response = await fetch(testUrl + code, {
 				method: "get",
@@ -29,22 +30,46 @@ const UserLoginQuery = selectorFamily({
 			});
 			if (400 <= response.status && response.status <= 599)
 				throw new Error("response is failed");
-			console.log(await response.json());
-			return response;
+			
+			const responseData = await response.text();
+			const json = responseData ? JSON.parse(responseData) : {}; // 응답 데이터가 있을 때만 JSON 파싱
+			console.log(json);
+			return json;
 		} catch (err) {
 			throw err;
 		}
 	}
 });
 
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires="+ d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function setRefreshToken(refreshToken) {
+	const expires = new Date();
+    expires.setDate(expires.getDate() + 14);
+	//setCookie("Authorized", refreshToken, {expires});
+	console.log("setcookie" + expires);
+	setCookie('refreshToken', refreshToken, expires);
+}
+
 const Auth = () => {
-	const [ params ] = useSearchParams();
+	const [params] = useSearchParams();
 	const code = params.get('code');
 	
 	// setCodeState(code);
 
 	const authRes = useRecoilValue(UserLoginQuery(code));
-	console.log(authRes);
+	console.log("AT : " + authRes.accessToken);
+	console.log("RT : " + authRes.refreshToken);
+
+	if (authRes.refreshToken)
+		setRefreshToken(authRes.refreshToken);
+	if (authRes.accessToken)
+		setAcessToken(authRes.accessToken);
 	return (
 		<div>
 			{authRes.status === 200 ? "Success Login" : "Failed Login"}
