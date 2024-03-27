97194358e9af266af42610625478e0148e94e69a
lm9204 <yeondeok99@naver.com>
2024-03-27 12:57:43 +0000

    feat: 로딩 Ui & 404 화면 추가

---
+++ src/components/redirect/Error.jsx
@@ -0,0 +1,16 @@
+
+import Error404 from './Error404';
+
+// error state
+const errorState = 404;
+
+const Error = () => {
+	return (
+		<div>
+			<Error404 />
+		</div>
+	);
+}
+
+
+export default Error;
---
+++ src/components/redirect/Error404.jsx
@@ -0,0 +1,9 @@
+const Error404 = () => {
+	return (
+		<>
+			<div className="error-background-404" />
+		</>
+	);
+}
+
+export default Error404;
