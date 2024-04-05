fe020d17db0c6e78bdfefb46af4a0ff2f9df20b6
lm9204 <yeondeok99@naver.com>
2024-04-05 13:27:07 +0000

    feat: 로그인, 투표 모달 추가

--- src/components/tournament/TournamentModal.jsx
+++ src/components/tournament/TournamentModal.jsx
@@ -9,36 +9,55 @@ const TournamentModal = () => {
 
 	return (
 		<>
-			{isopen && modalDataState.type === 'playerStat' && (
-				<div classNameName="modal" onClick={() => onClickDimmer()}>
-					<div classNameName="modal-content" onClick={(event) => event.stopPropagation()}>
-						<div classNameName="time-wrap">
+			{isopen && modalDataState.type === 'login' && (
+				<div className="modal" id="login-modal" onClick={() => onClickDimmer()}>
+					<div className="modal-content" onClick={(event) => event.stopPropagation()}>
+						<div className="time-wrap red">
 							<p>{modalDataState.title}</p>
 						</div>
-						<div classNameName="comment-wrap">
-							<p>{modalDataState.content}</p>
-						</div> 
+						<div className="comment-wrap">
+							{modalDataState.content}
+						</div>
+						<div className="select-wrap">
+							<div className="left" onClick={modalDataState.callback}>
+								<img src="42logo.svg" alt="42logo" id="logo-42" />
+								<span>로그인</span>
+							</div>
+							<div className="right" onClick={closeModal}>
+								<span>닫기</span>
+							</div>
+						</div>
+					</div>
+				</div>
+			)}
+			{isopen && modalDataState.type === 'playerStat' && (
+				<div className="modal" id="player-info-modal" onClick={() => onClickDimmer()}>
+					<div className="modal-content" onClick={(event) => event.stopPropagation()}>
+						{modalDataState.content}
 					</div>
 				</div>
 			)}
 			{isopen && modalDataState.type === 'voteSubmit' && (
-				<div classNameName="modal" onClick={() => onClickDimmer()}>
-					<div classNameName="modal-content" onClick={(event) => event.stopPropagation()}>
-						<div classNameName="time-wrap">
+				<div className="modal" onClick={() => onClickDimmer()}>
+					<div className="modal-content" onClick={(event) => event.stopPropagation()}>
+						<div className="time-wrap">
 							<p>{modalDataState.title}</p>
 						</div>
-						<div classNameName="comment-wrap">
+						<div className="name-wrap flex-row-start">
 							<p>{modalDataState.content}</p>
 						</div>
-						<div classNameName="select-wrap">
-							<div classNameName="left red" onClick={modalDataState.callback}>
-								<span classNameName="material-symbols-outlined">
-									cancel
-								</span>
+						<div className="warn-wrap flex-row-center red">
+							<span className="material-symbols-outlined">
+								gpp_maybe
+							</span>
+							<p>이후, 수정할 수 없습니다</p>
+						</div>
+						<div className="select-wrap">
+							<div className="left red" onClick={modalDataState.callback}>
 								<span >투표하기</span>
 							</div>
-							<div classNameName="right" onClick={closeModal}>
-								<span>취소</span>
+							<div className="right" onClick={closeModal}>
+								<span>닫기</span>
 							</div>
 						</div>
 					</div>
