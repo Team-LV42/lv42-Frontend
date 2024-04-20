f3735464b09b26fefe842f347a242cf97553c18a
lm9204 <yeondeok99@naver.com>
2024-04-20 19:52:10 +0000

    feat: 선택목록 컴포넌트

---
+++ src/store/addable.jsx
@@ -0,0 +1,26 @@
+import { selectorFamily } from 'recoil';
+
+import { deviceTotalSelectedMalfList } from './report';
+import { setSelected } from '../api/reportApi';
+
+export const getAddableContent = selectorFamily({
+	key: 'getAddableContent',
+	get: (index) => ({ get }) => {
+		const items = get(setSelected);
+ 		return items.controller_malf_btn_list[index] || false;
+	},
+	set: (index) => ({ set }, newValue) => {
+		set(deviceTotalSelectedMalfList, (prev) => {
+			let newItems = [...prev.controller_malf_btn_list];
+			if (!newValue)
+				newItems = newItems.filter((item, i) => i !== index);
+			else {
+				newItems[index] = newValue;
+			}
+			return {
+				...prev,
+				controller_malf_btn_list: newItems,
+			};
+		});
+	},
+});
