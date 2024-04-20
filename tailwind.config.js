e293c146e1bffdf47f3619ea029fa880abc49af0
lm9204 <yeondeok99@naver.com>
2024-04-20 19:50:51 +0000

    chore: tailwind bg 클래스 이미지 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,31 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    fontFamily: {
+      "noto" : ['Noto Sans KR', 'sans-serif'],
+      "outfit" : ['Outfit', 'sans-serif']
+    },
+    extend: {
+      screens: {
+        'pointerhover': {
+          'raw': "(hover:hover) and (pointer:fine)"
+        }
+      },
+      backgroundImage: {
+        'logo': "url('../public/logo/logo.svg')",
+        'logo-hazy': "url('../public/logo/logo-darkened.svg')",
+        'default': "url('../public/background.svg')",
+        'default-desktop': "url('../public/background-desktop.svg')",
+        'xbox': "url('../public/xbox.png')",
+        'switch': "url('../public/switch.png')",
+        'ps5': "url('../public/ps5.png')",
+        'controller-icon': "url('../public/controller-icon.svg')",
+        'ps5-btn-info': "url('../public/ps5-button-info2.png')",
+        'switch-btn-info': "url('../public/switch-button-info.png')",
+        'switch-pro-btn-info': "url('../public/switch-pro-button-info.png')",
+        'xbox-btn-info': "url('../public/xbox-button-info.png')"
+      }
+    },
   },
   plugins: [],
 }
