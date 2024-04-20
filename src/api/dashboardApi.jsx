a9283fec081ca6141c32ea66425a8c4b0e434f60
lm9204 <yeondeok99@naver.com>
2024-04-20 19:51:14 +0000

    fix: 관리자 대쉬보드 기능 수정

--- src/api/dashboardApi.jsx
+++ src/api/dashboardApi.jsx
@@ -1,9 +1,13 @@
 import { selector } from 'recoil';
 
+import { requestState } from '../pages/dashboard';
+
 export const getReportsList = selector({
 	key: 'GetReportsListSelector',
-	get: ({ get }) => async () => {
+	get: async ({ get }) => {
 		try {
+			const state = get(requestState);
+			if (state !== 'reports') return [];
 			const response = await fetch(`${process.env.REACT_APP_API_URL}/reports`, {
 				method: "GET",
 				headers: {
@@ -11,6 +15,8 @@ export const getReportsList = selector({
 					// "Authorization": `Bearer ${get(token)}`
 				},
 			});
+
+			return await response.json();
 		} catch (error) {
 			console.log(error);
 			return [];
@@ -18,13 +24,34 @@ export const getReportsList = selector({
 	}
 });
 
-export const postMalfType_TEST = async ({ name, description }) => {
+export const postNewDevice = async (deviceId, deviceName, consoleType, deviceType, deviceStatus) => {
+	try {
+		if (!deviceId || !consoleType || !deviceStatus || !deviceName || !deviceType) {
+			console.log("postNewDevice: invailed input format");
+			return false;
+		}
+		
+		const response = await fetch(`${process.env.REACT_APP_API_URL}/devices?id=${deviceId}&console_id=${consoleType}&name=${deviceName}&device_type=${deviceType}&status=${deviceStatus}`, {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+		});
+		
+		return await response.json();
+	} catch (error) {
+		console.log(error);
+		return false;
+	}
+};
+
+export const postNewMalfType_TEST = async (name, description) => {
 	try {
 		if (!name || !description) {
 			console.log("postMalfType_TEST: invailed input format");
 			return false;
 		}
-		const response = await fetch(`${process.env.REACT_APP_API_URL}/reports/testMalfunctionType?name=${name}&description=${description}`, {
+		const response = await fetch(`${process.env.REACT_APP_API_URL}/reports/types/malfunction_type?name=${name}&description=${description}`, {
 			method: "POST",
 			headers: {
 				"Content-Type": "application/json",
@@ -39,13 +66,13 @@ export const postMalfType_TEST = async ({ name, description }) => {
 	}
 };
 
-export const postBtnMalfType_TEST = async ({ name, description }) => {
+export const postNewBtnMalfType_TEST = async (name, description) => {
 	try {
 		if (!name || !description) {
 			console.log("postBtnMalfTytpe_TEST: invailed input format");
 			return false;
 		}	
-		const response = await fetch(`${process.env.REACT_APP_API_URL}/reports/testButtonMalfunctionType?name=${name}&description=${description}`, {
+		const response = await fetch(`${process.env.REACT_APP_API_URL}/reports/types/button_malfunction_type?name=${name}&description=${description}`, {
 			method: "POST",
 			headers: {
 				"Content-Type": "application/json",
@@ -60,13 +87,13 @@ export const postBtnMalfType_TEST = async ({ name, description }) => {
 	}
 };
 
-export const updateDeviceStatus_TEST = async ({ id, status }) => {
+export const updateDeviceStatus_TEST = async (id, status ) => {
 	try {
 		if (!id || !status) {
 			console.log("updateDeviceStatus_TEST: invailed input format");
 			return false;
 		}
-		const response = await fetch(`${process.env.REACT_APP_API_URL}/devices/testUpdateDeviceStatus?id=${id}&status=${status}`, {
+		const response = await fetch(`${process.env.REACT_APP_API_URL}/devices?id=${id}&status=${status}`, {
 			method: "PATCH",
 			headers: {
 				"Content-Type": "application/json",
@@ -79,4 +106,51 @@ export const updateDeviceStatus_TEST = async ({ id, status }) => {
 		console.log(error);
 		return false;
 	}
+}
+
+
+export const deleteDevice = async (device_id) => {
+	const response = await fetch(`${process.env.REACT_APP_API_URL}/devices`, {
+		method: 'DELETE',
+		headers: {
+			'Content-Type': "application/json",
+		}
+	})
+
+	if (response.status === 200) {
+		console.log('성공');
+	}
+	else
+		console.log('실패');
+};
+
+export const deleteReport = async (report_id) => {
+	const response = await fetch(`${process.env.REACT_APP_API_URL}/reports`, {
+		method: 'DELETE',
+		headers: {
+			'Content-Type': "application/json",
+		}
+	})
+
+	if (response.status === 200) {
+		console.log('성공');
+	}
+	else
+		console.log('실패');
+}
+
+export const updateReport = async (report_id, status) => {
+	if (!report_id || !status) return false;
+	const response = await fetch(`${process.env.REACT_APP_API_URL}/reports?id=${report_id}&status=${status}`, {
+		method: 'PATCH',
+		headers: {
+			'Content-Type': "application/json",
+		}
+	})
+
+	if (response.status === 200) {
+		console.log('성공');
+	}
+	else
+		console.log('실패');
 }
--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -1,14 +1,27 @@
-import { useEffect } from 'react';
-import { useSetRecoilState } from 'recoil';
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import {
+	atom,
+	selector,
+	useRecoilState,
+	useSetRecoilState,
+	useRecoilValue,
+} from 'recoil';
 
 import ToggleList from '../components/ToggleList';
+import AddDeviceForm from '../components/dashboard/AddDevice';
+import UpdateDeviceForm from '../components/dashboard/UpdateDevice';
 import { toggleState } from '../store/toggle';
+import { controllerType, deviceTypeState, deviceIDState } from '../store/report';
 import '../styles/dashboard.css';
+import { deviceListSelector } from '../api/reportApi';
+import { getReportsList, deleteDevice, deleteReport } from '../api/dashboardApi';
 
 const TestToggle = {
 	0: {
 		title: '[TEST] 테스트 기기 추가',
-		content: '', 
+		content: <AddDeviceForm />, 
 		isVisible: false,
 	},
 	1: {
@@ -23,14 +36,62 @@ const TestToggle = {
 	},
 	3: {
 		title: '[TEST] 기기 상태 수정',
-		content: 'Toggle content 5',
+		content: <UpdateDeviceForm />,
 		isVisible: false,
 	},
 };
 
+export const requestState = atom({
+	key: 'RequestState',
+	default: 'none',
+})
+
+const IssueList = ({ list }) => {
+	const request = useRecoilValue(requestState);
+
+	console.log(list);
+
+	return (
+		<>
+		{list.length !== 0 && request === 'reports' && list.map((report, index) => {
+			return (
+				<div className='option' key={index}>
+					<span>_ID: {report._id} </span>
+					<span>Device ID: {report.device} </span>
+					<span>Console: {report.console_type} </span>
+					<span>기타 설명: {report.etc_description} </span>
+					<span>상태: {report.status} </span>
+					<p>생성 일시: {report.createdAt} </p>
+					<p>수정 일시: {report.updatedAt} </p>
+					<p>고장 접수 내용</p>
+					<div>
+						{report.controller_malf_btn_list.map((malf, index) => (
+							<div key={malf._id}>
+								<span>고장버튼 ID: {malf.controller_btn_id} </span>
+								<span>고장증상 : {malf.controller_btn_malf_type} </span>
+							</div>
+						))}
+					</div>
+					<button onClick={() => deleteReport(report._id)}>신고 삭제</button>
+				</div>
+			)
+		})}
+		</>
+	)
+}
+
 
 const Dashboard = () => {
+	const navigate = useNavigate();
 	const setToggleState = useSetRecoilState(toggleState);
+	const [request, setRequest] = useRecoilState(requestState);
+
+	const [consoleType, setConsoleType] = useRecoilState(controllerType);
+	const [deviceType, setDeviceType] = useRecoilState(deviceTypeState);
+
+	const [deviceId, setDeviceId] = useRecoilState(deviceIDState);
+	const deviceList = useRecoilValue(deviceListSelector);
+	const issueList = useRecoilValue(getReportsList);
 	
 	const addToggleItem = () => {
 		setToggleState((prev) => ({
@@ -41,31 +102,12 @@ const Dashboard = () => {
 			isVisible: false,
 		  },
 		}));
-	  };
-	
-	const postNewDevice = async ({ deviceId, consoleType, deviceStatus }) => {
-		try {
-			if (!deviceId || !consoleType || !deviceStatus) {
-				console.log("postNewDevice: invailed input format");
-				return false;
-			}
-			
-			const response = await fetch(`${process.env.REACT_APP_JUNKIM2}/devices?id=${deviceId}&console_id=${consoleType}&status=${deviceStatus}`, {
-				method: "POST",
-				headers: {
-					"Content-Type": "application/json",
-				},
-			});
-			
-			return await response.json();
-		} catch (error) {
-			console.log(error);
-			return false;
-		}
 	};
 
+	console.log(request);
+
 	useEffect(() => {
-		setToggleState(TestToggle);	
+		setToggleState(TestToggle);
 	}, []);
 	
 	return (
@@ -75,30 +117,54 @@ const Dashboard = () => {
 					<div className="section">
 						<h3>#기기 목록</h3>
 						<div className="options">
-							<button className="option">XBox</button>
-							<button className="option">Switch</button>
-							<button className="option">PS5</button>
+							<button className="option" onClick={() => {setConsoleType(1); setRequest('none')}}>XBox</button>
+							<button className="option" onClick={() => {setConsoleType(2); setRequest('none')}}>Switch</button>
+							<button className="option" onClick={() => {setConsoleType(3); setRequest('none')}}>PS5</button>
 						</div>
+						{consoleType !== 0 && request === 'none' && (
+							<div className="options">
+								<button className="option" onClick={() => setDeviceType(1)}>본체</button>
+								<button className="option" onClick={() => setDeviceType(2)}>컨트롤러</button>
+								<button className="option" onClick={() => setDeviceType(3)}>기타</button>
+							</div>
+						)}
+						{deviceList.length !== 0 && request === 'none' && (
+							<div className="device-list">
+								{deviceList.map((device) => (
+									<div
+									key={device.id}
+									className={`device-item option ${deviceId === device.id ? 'selected' : ''}`}
+									onClick={() => deviceId === device.id
+										? setDeviceId('')
+										: setDeviceId(device.id)
+									}
+									>
+										<div>{device.id}</div>
+										<div>{device.name}</div>
+										<div>{device.console_id}</div>
+										<div>{device.device_type}</div>
+										<div>{device.status}</div>
+									</div>
+								))}
+							</div>
+						)}
+						{/* <button onClick={deleteDevice}>삭제</button> */}
 						</div>
 						<div className="section">
-						<h3>#고장 신고 <button className='sml-option'>조회</button></h3>
-						<div className="options">
-							<button className="option">본체</button>
-							<button className="option">컨트롤러</button>
-							<button className="option">기타</button>
-						</div>
+							<h3>#고장 신고 <button className='sml-option' onClick={() => setRequest('reports')}>조회</button></h3>
+							<IssueList list={issueList} />
 						</div>
 						<div className="section">
-						<h3>#고장 유형 <button className='sml-option'>조회</button></h3>
-						<div className="options">
-							<button className="option">1</button>
-							<button className="option">2</button>
-							<button className="option">3</button>
-							<button className="option">4</button>
-							<button className="option">5</button>
-							<button className="option">6</button>
+							<h3>#고장 유형 <button className='sml-option'>조회</button></h3>
+							<div className="options">
+								<button className="option">1</button>
+								<button className="option">2</button>
+								<button className="option">3</button>
+								<button className="option">4</button>
+								<button className="option">5</button>
+								<button className="option">6</button>
+							</div>
 						</div>
-					</div>
 					<div className="section">
 						<h3>#테스트 기능</h3>
 						<ToggleList />
