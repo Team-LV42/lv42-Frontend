562951692cdeae457fbafa887e2711086938d8b2
lm9204 <yeondeok99@naver.com>
2024-04-22 06:34:19 +0000

    chore: 컨트롤러 아이콘 이미지 수정 및 애니메이션 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,18 @@ module.exports = {
       "outfit" : ['Outfit', 'sans-serif']
     },
     extend: {
+      animation: {
+        shake: 'shake .5s ease-in-out infinite'
+      },
+      keyframes: {
+        shake: {
+          '0%': { transform: 'translateX(0)' },
+          '25%': { transform: 'translateX(-5px)' },
+          '50%': { transform: 'translateX(5px)' },
+          '75%': { transform: 'translateX(-2.5px)' },
+          '100%': { transform: 'translateX(0)' }
+        }
+      },
       screens: {
         'pointerhover': {
           'raw': "(hover:hover) and (pointer:fine)"
@@ -22,7 +34,7 @@ module.exports = {
         'xbox': "url('../public/xbox.png')",
         'switch': "url('../public/switch.png')",
         'ps5': "url('../public/ps5.png')",
-        'controller-icon': "url('../public/controller-icon.svg')",
+        'controller-icon': "url('../public/controller-icon2.svg')",
         'ps5-btn-info': "url('../public/ps5-button-info2.png')",
         'switch-btn-info': "url('../public/switch-button-info.png')",
         'switch-pro-btn-info': "url('../public/switch-pro-button-info.png')",
