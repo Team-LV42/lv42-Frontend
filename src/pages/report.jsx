362a1cb9fac5343255f50033ade1c5cb026c319e
lm9204 <yeondeok99@naver.com>
2024-05-05 13:20:35 +0000

    fix: 고장문의 페이지 검색버튼 삭제

--- src/pages/report.jsx
+++ src/pages/report.jsx
@@ -72,7 +72,7 @@ const DeviceHeader = ({ page }) => {
 						<path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
 					</svg>
 				</button>
-				<button id="search-buttom" class="flex" onClick={() => openModal({type: 'search'})}>
+				<button id="search-buttom" class="invisible flex" onClick={() => openModal({type: 'search'})}>
 					<svg class="w-7 h-7" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
 						<path stroke-linecap="round" stroke-linejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
 					</svg>
