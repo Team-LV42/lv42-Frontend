c78e88e9fbb3db80b61016437b302d4e640878c7
lm9204 <yeondeok99@naver.com>
2024-05-06 13:07:58 +0000

    feat: 현재예약 진행도 추가

--- src/components/Record.jsx
+++ src/components/Record.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useRecoilValue } from 'recoil';
 
@@ -8,13 +9,16 @@ import useToken from '../hooks/useToken';
 import { deleteModal } from '../store/Modal';
 import { deleteBookRecord } from '../api/bookApi';
 import { bookSelector } from '../store/book';
+import { useEffect } from 'react';
 
 export const Record = ({ index, type, state, onClick, isDeletable = false, isSelected = false }) => {
 	const { openModal, closeModal } = useModal();
-	const { tickToTime, getDuration, curTick } = useDate();
+	const { tickToTime, getDuration, curTick, setUpdateTick } = useDate();
 	const navigate = useNavigate();
 	const accessToken = useToken().accessToken();
 	const record = useRecoilValue(bookSelector({type, index}));
+
+	const [bgcolor, setBgcolor] = useState('');
 
 	const deleteAction = async () => {
 		await deleteBookRecord(record._id, record.user_id, accessToken);
@@ -33,9 +37,6 @@ export const Record = ({ index, type, state, onClick, isDeletable = false, isSel
 			case 3:
 				str = 'PS5';
 				break ;
-			default :
-				str = '';
-				break ;
 		}
 		if (flag)
 			return str.toLowerCase();
@@ -46,30 +47,33 @@ export const Record = ({ index, type, state, onClick, isDeletable = false, isSel
 		if (curTick > index)
 			return ('-');
 		return (isSelected ? 'SELECTED' : '슬롯 선택하기');
-	}
+	};
 
 	const slotSelectedStyles = {
 		'xbox': 'slot-selected-xbox',
 		'switch': 'slot-selected-switch',
 		'ps5' : 'slot-selected-ps5',
+	};
+
+	const calcurateTimeDifference = (slotTick, curTick) => {
+		const now = new Date();
+		if (slotTick !== curTick)
+			return false;
+		else
+			return Math.floor(now.getMinutes() / 10) % 3 / 3 * 100;
 	}
+
+	useEffect(() => {
+		setUpdateTick(true);
+	}, []);
 	
+	useEffect(() => {
+		const newTime = calcurateTimeDifference(index, curTick);
+		setBgcolor(`linear-gradient(rgb(198, 198, 198) ${newTime}%, white 10%)`);
+	}, [curTick]);
+
 	return (
 		<>
-			{state === 'user' && record !== null && (
-				<span
-				className="slot"
-				value={index}
-				onClick={(event) => {
-					event.preventDefault();
-					(isDeletable && openModal(deleteModal));
-				}}
-				// onClick="showModal('reservationModal')"
-			>
-				<div className="slot-time"><p>{record.date}</p><p>{getDuration(record.start_time, record.end_time)}</p></div>
-				<div className={`slot-value content-type-${record.type}`}><p>{typeToString(record.type, 0)}</p></div>
-			</span>
-			)}
 			{state === 'book' && record === null && (
 				<div
 				value={index}
@@ -78,8 +82,8 @@ export const Record = ({ index, type, state, onClick, isDeletable = false, isSel
 					event.preventDefault();
 					onClick(event);
 				}}
-				//${curTick > index ? 'disabled' : ''} 
 				class={`${isSelected ? slotSelectedStyles[typeToString(type, 1)] : 'slot-empty'} ${curTick > index ? 'slot-elapsed' : ''} w-full h-[4.5rem] flex flex-row items-center justify-around px-4 border-b border-[#C1C1C1]`}
+				style={{ backgroundImage: curTick === index && bgcolor}}
 				>
 					<div class="w-36 h-full flex items-center justify-center text-lg font-bold px-2 font-outfit">
 						<p>{tickToTime(index)}~</p>
@@ -99,6 +103,7 @@ export const Record = ({ index, type, state, onClick, isDeletable = false, isSel
 					{isDeletable || navigate(`/user/${record.user_id}`)}
 				}}
 				class={` ${curTick > index ? 'slot-elapsed' : ''} w-full h-[4.5rem] flex flex-row items-center justify-around px-4 border-b border-[#C1C1C1] slot-full`}
+				style={{ backgroundImage: curTick === index && bgcolor }}
 				>
 					<div class="w-36 h-full flex items-center justify-center text-lg font-bold px-2 font-outfit">
 						<p>{tickToTime(index)}~</p>
