26b1ce92eb5d80ce21a6c6d726cd5efc1c9da9b9
김준호 <junkim2@student.42seoul.kr>
2024-05-07 12:23:02 +0900

    fix: fixed css bug

--- src/components/onboarding/OnBoardingConsoleGuide.jsx
+++ src/components/onboarding/OnBoardingConsoleGuide.jsx
@@ -36,25 +36,26 @@ const OnBoardingConsoleGuide = ({ closeModal, data, user }) => {
 					<div class="w-full h-full flex flex-col items-center justify-center py-10 min-[376px]-[gap-6] gap-2">
 						<div class="w-full flex flex-col items-center justify-center h-10">
 							<p class="text-3xl font-bold mb-2">예약 성공</p>
-							<p class="text-sm font-medium">{content[data.content][0]}</p>
 						</div>
-						<div class="w-full flex flex-col items-center justify-center mb-10">
+						<div class="w-full flex flex-col items-center justify-center gap-5">
 							<p class="text-xl font-bold mb-2">어떻게 게임을 시작하나요?</p>
 							<p class="text-sm font-medium">{content[data.content][1]}</p>
 							<p class="text-sm font-medium mb-2">콘솔의 전원을 켜주세요.</p>
 							<span class={`w-44 h-24 bg-basic ${consoleBgStyles[data.content]}`}></span>
 						</div>
-						<div class="w-full flex flex-col items-center justify-center mb-6">
+						<div class="w-full flex flex-col items-center justify-center">
 							<p class="text-xl font-bold mb-2">어떻게 게임을 종료하나요?</p>
 							<p class="text-sm font-medium">{content[data.content][2]}</p>
-							<p class="text-sm font-medium mb-2">{content[data.content][3]}</p>
+							<p class="text-sm font-medium">{content[data.content][3]}</p>
 						</div>
-						<button onClick={() => {navigate('/');closeModal()}} class="w-[210px] h-[46px] mb-6 rounded-3xl bg-gray-300 pointerhover:hover:bg-gray-400 font-semibold text-[17px]">
-							예약 홈으로 돌아가기
-						</button>
-						<button onClick={() => {navigate(`/user/${user.id}`);closeModal()}} class="w-[210px] h-[46px] rounded-3xl bg-gray-300 pointerhover:hover:bg-gray-400 font-semibold text-[17px]">
-							내 예약 바로가기
-						</button>
+						<div class="w-full min-[376px]-h-28 h-[6.5rem] flex flex-col items-center justify-between">
+							<button onClick={() => {navigate('/');closeModal()}} class="w-[210px] h-[46px] rounded-3xl bg-gray-300 pointerhover:hover:bg-gray-400 font-semibold text-[17px]">
+								예약 홈으로 돌아가기
+							</button>
+							<button onClick={() => {navigate(`/user/${user.id}`);closeModal()}} class="w-[210px] h-[46px] rounded-3xl bg-gray-300 pointerhover:hover:bg-gray-400 font-semibold text-[17px]">
+								내 예약 바로가기
+							</button>
+						</div>
 					</div>
 				</div>
 				  
